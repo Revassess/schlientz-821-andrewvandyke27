d7c8b8f61c8b0567e916892557fad11fdaf7b9f8
Andrew VanDyke <andrew.vandyke@revature.net>
2020-08-04 15:57:08 -0400

    tier 1 done

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,15 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
+    	
+    	if(s1.length() == s2.length()) {
+    		for(int i = 0; i < s1.length(); i++) {
+    			if(s1.charAt(i) != s2.charAt(i)) {
+    				return false;
+    			}
+    		}
+    		return true;
+    	}
         return false;
     }
 }
--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,19 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		
+		String n = Long.toString(num);
+		int length = n.length();
+		int sum = 0;
+		
+		for(int i = 0; i < length; i++) {
+			String s = n.charAt(i) + "";
+			sum += Math.pow(Integer.parseInt(s), length);
+		}
+		
+		if(sum == num)
+			return true;
+		
 		return false;
 	}
 }
