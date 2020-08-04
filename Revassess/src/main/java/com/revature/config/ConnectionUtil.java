83942acb7c754f1fee9dc0568b4f18a49d0c6e0c
Andrew VanDyke <andrew.vandyke@revature.net>
2020-08-04 18:31:49 -0400

    tier 3 i guess

--- Revassess/src/main/java/com/revature/config/ConnectionUtil.java
+++ Revassess/src/main/java/com/revature/config/ConnectionUtil.java
@@ -1,6 +1,11 @@
 package com.revature.config;
 
+import java.io.FileInputStream;
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import util.JDBCConnection;
 
 /**
  * 
@@ -16,18 +21,33 @@ public class ConnectionUtil {
 	private static ConnectionUtil cu;
 	
 	// add your jdbc url
-	public static final String URL = "";
+	public static final String URL = "jdbc:oracle:thin:@drewdata.c2os8rtic6ai.us-east-2.rds.amazonaws.com:1521:ORCL";
 	// add your jdbc username
-	public static final String USERNAME = "";
+	public static final String USERNAME = "drew";
 	// add your jdbc password
-	public static final String PASSWORD = "";
+	public static final String PASSWORD = "password";
 	// name of the created stored procedure in tier 3
 	public static final String TIER_3_PROCEDURE_NAME = "";
 	// name of the created sequence in tier 3
-	public static final String TIER_3_SEQUENCE_NAME = "";
+	public static final String TIER_3_SEQUENCE_NAME = "rev_seq";
 
 	// implement this method to connect to the db and return the connection object
+	private static Connection conn = null;
 	public Connection connect(){
+		try {
+			if(conn == null) {
+
+				Class.forName("oracle.jdbc.driver.OracleDriver");
+				
+				conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+				return conn;
+				
+			} else {
+				return conn;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return null;
 	}
 
